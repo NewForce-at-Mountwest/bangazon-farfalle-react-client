afcf53c5a60e6fba1182d4479a49702d85b031b3
Cluttered04 <sbpwem@gmail.com>
2019-05-23 14:39:23 -0400

    bug fixing

--- src/components/Employees/EmployeeCard.js
+++ src/components/Employees/EmployeeCard.js
@@ -8,15 +8,16 @@ class EmployeeCard extends Component {
 
 
     render() {
+
         return(
            <div>
                <div className="card" style={{width: "18rem"}}>
                 <img src="https://www.milcorp.com/assets/employee-placeholder-male.jpg" className="card-img-top" alt="Employee photo"/>
                 <div className="card-body">
-                    <h5 className="card-title">{this.props.employee.firstName}{this.props.employee.lastName}</h5>
+                    <h5 className="card-title">{this.props.employee.firstName}      {this.props.employee.lastName}</h5>
                     <h6 className="card-text">Department: {this.props.employee.departmentId}</h6>
-                    {/* <h6>Currently Assigned Computer: {this.props.employee.currentComputer.id}</h6>
-                    <p>{this.props.employee.currentComputer.make}</p><p>{this.props.employee.currentComputer.manufacturer}</p> */}
+                    <h6>Currently Assigned Computer: {this.props.employee.currentComputer ?this.props.employee.currentComputer.id : ""}</h6>
+                    <p>{this.props.employee.currentComputer ?this.props.employee.currentComputer.make : ""}</p><p>{this.props.employee.currentComputer ?this.props.employee.currentComputer.manufacturer: ""}</p>
                     <button onClick={()=> this.props.history.push(`/employees/${this.props.employee.id}/edit`)}>Edit Employee</button>
                 </div>
 
